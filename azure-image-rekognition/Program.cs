46f61436d43506e4ae08739d0897d74fcf86c6f1
Alex Bierhaus <abierhaus@compeon.de>
2020-10-29 10:06:46 +0100

    clean up

--- azure-image-rekognition/Program.cs
+++ azure-image-rekognition/Program.cs
@@ -14,16 +14,13 @@ namespace azure_image_rekognition
         private static readonly string subscriptionKey = "Your API Key here";
         private static readonly string endpoint = "https://yourendpoint.cognitiveservices.azure.com/";
 
-        // the Analyze method endpoint
-
-
         private static async Task Main(string[] args)
         {
             // Call the API
             await Detect();
         }
 
-      
+
         private static async Task Detect()
         {
             var client = new HttpClient();
@@ -33,25 +30,25 @@ namespace azure_image_rekognition
                 "Ocp-Apim-Subscription-Key", subscriptionKey);
 
 
-            //Add parameters that should be returned. Possible Parameters: Categories, Description, Color
+            // Add parameters that should be returned. Possible Parameters: Categories, Description, Color
             var requestParameters =
                 "visualFeatures=Description";
 
             // Assemble the URI for the REST API method.
             var uri = $"{endpoint}vision/v3.1/analyze?{requestParameters}";
 
-            
-            //Get picture from local storage
+
+            // Get picture from local storage
             var photo = "img/sheep.png";
             await using var fs = new FileStream(photo, FileMode.Open, FileAccess.Read);
             var binaryReader = new BinaryReader(fs);
             var byteData = binaryReader.ReadBytes((int) fs.Length);
 
-            
+
             HttpResponseMessage response;
             using (var content = new ByteArrayContent(byteData))
             {
-               content.Headers.ContentType =
+                content.Headers.ContentType =
                     new MediaTypeHeaderValue("application/octet-stream");
 
                 // Asynchronously call the REST API method.
@@ -64,7 +61,6 @@ namespace azure_image_rekognition
             var detectLabelResponse = JsonConvert.DeserializeObject<DetectLabelResponse>(contentString);
 
             Console.WriteLine("Detected labels for " + photo);
-            
             foreach (var label in detectLabelResponse.Description.Tags) Console.WriteLine(label);
         }
 
